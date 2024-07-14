6325cf372f8fc3317f2fc6b025d9ab1b49870664
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-07-14 10:05:58 +0530

    added config

--- src/init/firebase.js
+++ src/init/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { GoogleAuthProvider, getAuth, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, signOut, } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
